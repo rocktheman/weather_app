1bc0479838d8c29408ee7c6e404d0b18d321176f
Hassan <45733306+rocktheman@users.noreply.github.com>
2021-11-08 11:44:04 +0500

    latest

---
+++ lib/models.dart
@@ -0,0 +1,3 @@
+{"coord":{"lon":67.0822,"lat":24.9056},
+"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50d"}],"base":"stations","main":{"temp":304.05,"feels_like":302.11,"temp_min":304.05,"temp_max":304.05,"pressure":1015,"humidity":19},"visibility":3000,"wind":{"speed":3.6,"deg":70},"clouds":{"all":5},"dt":1636353031,"sys":{"type":1,"id":7576,"country":"PK","sunrise":1636335801,
+"sunset":1636375651},"timezone":18000,"id":1174872,"name":"Karachi","cod":200}
