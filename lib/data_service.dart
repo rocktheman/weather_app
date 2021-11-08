8a3f21161163b79e1072e4e5cf50b7822f24295f
Hassan <45733306+rocktheman@users.noreply.github.com>
2021-11-08 11:22:50 +0500

    ok

--- lib/data_service.dart
+++ lib/data_service.dart
@@ -1,14 +1,14 @@
 //import 'dart:convert';
 import 'package:http/http.dart'as http;
 class DataService{
-     void getWeather(String city ){
+     void getWeather(String city )async{
           final queryParameters ={
            'q':city,
            'appid':'1b6e509e4cfc33a48debde6ac5ed4005',
   };
 
   final uri = Uri.https('api.openweathermap.org', '/data/2.5/weather',queryParameters);
-  final response = http.get(uri);
+  final response = await http.get(uri);
   print(response.body);
     
   
