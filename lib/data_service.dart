dd972589e642b1228a1a84c95e44af262e050ad4
Hassan <45733306+rocktheman@users.noreply.github.com>
2021-11-09 11:30:04 +0500

    9-11-2021

--- lib/data_service.dart
+++ lib/data_service.dart
@@ -1,17 +1,22 @@
 //import 'dart:convert';
-import 'package:http/http.dart'as http;
-class DataService{
-     void getWeather(String city )async{
-          final queryParameters ={
-           'q':city,
-           'appid':'1b6e509e4cfc33a48debde6ac5ed4005',
-  };
+import 'dart:convert';
 
-  final uri = Uri.https('api.openweathermap.org', '/data/2.5/weather',queryParameters);
-  final response = await http.get(uri);
-  print(response.body);
-    
-  
-}
+import 'package:http/http.dart' as http;
+import 'package:weather_app/models.dart';
 
+class DataService {
+  Future getWeather(String city) async {
+    final queryParameters = {
+      'q': city,
+      'appid': '1b6e509e4cfc33a48debde6ac5ed4005',
+    };
+
+    final uri = Uri.https(
+        'api.openweathermap.org', '/data/2.5/weather', queryParameters);
+    final response = await http.get(uri);
+    //print(response.body);
+
+    final json = jsonDecode(response.body);
+    return weatherResponse.fromJson(json);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,74 +3,52 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/data_service.dart';
 
-
 void main() {
   runApp(MyApp());
- 
 }
-class MyApp extends StatefulWidget{
- 
- 
- @override
-State<MyApp> createState()=> _MyAppState();
 
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  
-
-  }
-
-  
-
-
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   final _dataService = DataService();
 
   final _cityTextController = TextEditingController();
-@override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          body: Center (
-            child: Column( mainAxisAlignment: MainAxisAlignment.center, children:[
-            
-            Padding(
-             padding: EdgeInsets.symmetric(vertical: 30),
-             child: SizedBox(
-               width: 150,
-               child: TextField(
-                 controller: _cityTextController,
-                 decoration: InputDecoration(
-                   labelText: 'City',
-                   hintText: 'Please enter valid city name',
-                 ),
-                 textAlign:TextAlign.center,
-                 // ignore: prefer_const_constructors
-                 ),
-             ), 
-             ),
-            ElevatedButton(
-              onPressed: _search, 
-              child: Text('Search')
-              
-            ),
-             ],
-            
-            ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 30),
+                child: SizedBox(
+                  width: 150,
+                  child: TextField(
+                    controller: _cityTextController,
+                    decoration: InputDecoration(
+                      labelText: 'City',
+                      hintText: 'Please enter valid city name',
+                    ),
+                    textAlign: TextAlign.center,
+                    // ignore: prefer_const_constructors
+                  ),
+                ),
+              ),
+              ElevatedButton(onPressed: _search, child: Text('Search')),
+            ],
+          ),
         ),
-    ),
+      ),
     );
-    
-  }
-  void _search(){
-    _dataService.getWeather(_cityTextController.text);
   }
 
-
-
-
+  void _search() async {
+    final response = await _dataService.getWeather(_cityTextController.text);
+    print(response.cityName);
+  }
 }
-
-
-
-
-
